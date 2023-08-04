07a8d2141613747f39459a738050bd88f477dce1
Abhay-Chand <113808591+Abhay-Chand@users.noreply.github.com>
2023-08-04 22:48:37 +0530

    Add files via upload

---
+++ data_type.py
@@ -0,0 +1,6 @@
+a=10000
+b="string"
+c=True
+print(type(a))
+print(type(b))
+print(type(c))
---
+++ type_conversion.py
@@ -0,0 +1,5 @@
+num_dd= input("enter your name :")
+print(num_dd)
+
+new_num_dd=str(len(num_dd))
+print("you have " + new_num_dd + " character in your Name.")
