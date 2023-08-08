6b48325535a65b0e2aed3f8a6b3ea03ba1ef5e42
Abhay-Chand <113808591+Abhay-Chand@users.noreply.github.com>
2023-08-08 23:45:02 +0530

    Add files via upload

---
+++ list_item.py
@@ -0,0 +1,15 @@
+#list item are ordered,changeable,and allow duplication
+#list item are indexed,the first item has index [0],and second item  has index[1]
+this_list = ["apple","banana","cherry","apple"] #Dupliction
+print(this_list)
+print(len(this_list))
+#list datatype
+list1=["anbahaly","lsjc","xascas"]
+list2=[12,3132,15,153,5,5]
+list3=[True,False,True]
+print(list1[0])
+#list can contain different datatype at once
+list4 = ["abc",32,True,40,"male"]
+print(list4)
+print(type(list4))
+print(type(this_list))
